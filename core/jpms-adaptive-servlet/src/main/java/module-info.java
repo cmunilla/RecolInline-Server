562fb62a16babc6fb73f729e261c6946a4b4d497
cmunilla <cmunilla@cmssi-fr>
2020-08-01 09:39:54 +0200

    Introduce jpms-adaptive-servlet module

---
+++ core/jpms-adaptive-servlet/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * @author cmunilla@cmssi.fr
+ * @version 0.3
+ */
+open module jpms.adaptive.servlet {
+
+	exports javax.servlet;
+	exports javax.servlet.annotation;
+	exports javax.servlet.descriptor;
+	exports javax.servlet.http;
+	
+}
